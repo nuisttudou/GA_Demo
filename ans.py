92974dda109c25190c8584fcb6bd05c81169b6ac
tudou <20171308085@nuist.edu.cn>
2019-12-03 21:49:32 +0800

    third commit

--- ans.py
+++ ans.py
@@ -13,15 +13,18 @@ Created on Tue Dec  3 16:21:00 2019
 
 @author: tudou
 """
+
+NUM_OF_PLANE=11
+NUM_OF_G=4
 import pandas as pd
 import numpy as np
 import time
 from pyeasyga import pyeasyga
 def create_individual(data):
     data=plane_data_in
-    x=np.zeros((4, 11))
-    ran=np.array([0 for _ in range(0,11)])    
-    for i in range(0,11):
+    x=np.zeros((NUM_OF_G, NUM_OF_PLANE))
+    ran=np.array([0 for _ in range(0,NUM_OF_PLANE)])    
+    for i in range(0,NUM_OF_PLANE):
         s=0
         l=[]
         for j in range(0,i):
@@ -41,21 +44,21 @@ def fitness(individual, data):
     values=0
     x=individual
     x=np.array(x)
-    x.shape=(4,11)
-    for i in range(0,11):
-        for j in range(0,4):
+    x.shape=(NUM_OF_G,NUM_OF_PLANE)
+    for i in range(0,NUM_OF_PLANE):
+        for j in range(0,NUM_OF_G):
             if x[j,i]:
                 values+=np.sum(data[i,2:5]*G_data_in[j,:])
     def check(xx):
-        for ii in range(0,11):
+        for ii in range(0,NUM_OF_PLANE):
             if np.sum(xx[:,ii])!=1:
                 return False
-        ran=np.array([0 for _ in range(0,11)])
-        for i in range(0,11):
-            for j in range(0,4):
+        ran=np.array([0 for _ in range(0,NUM_OF_PLANE)])
+        for i in range(0,NUM_OF_PLANE):
+            for j in range(0,NUM_OF_G):
                 if xx[j,i]==1:
                     ran[i]=j
-        for i in range(0,11):
+        for i in range(0,NUM_OF_PLANE):
             for j in range(0,i):
                 if data[j,1]>data[i,0] and ran[i]==ran[j]:
                     return False  
@@ -66,15 +69,15 @@ def fitness(individual, data):
 t_in=time.time()
 plane_data_in=np.array(pd.read_csv('./plane.csv',header=None))
 G_data_in=np.array(pd.read_csv('./G.csv',header=None))
-plane_4=np.r_[plane_data_in,plane_data_in,plane_data_in,plane_data_in]
-#ga = pyeasyga.GeneticAlgorithm(plane_4,population_size=200,
+plane_NUM_OF_G=np.r_[plane_data_in,plane_data_in,plane_data_in,plane_data_in]
+#ga = pyeasyga.GeneticAlgorithm(plane_NUM_OF_G,population_size=200,
 #                               generations=200,maximise_fitness=False)#,elitism=True
 #
 ga = pyeasyga.GeneticAlgorithm(data,
                                population_size=50,
                                generations=50,
                                crossover_probability=0.8,
-                               mutation_probability=0.1,
+                               mutation_probability=0.05,
                                elitism=True,
                                maximise_fitness=False)
 
@@ -85,19 +88,19 @@ ga.run()                                    # run the GA
 print(ga.best_individual())                  # print the GA's best solution
 t=np.array(ga.best_individual()[1])
 
-t.shape=(4,11)
+t.shape=(NUM_OF_G,NUM_OF_PLANE)
 print(t)
 s=0
-for i in range(0,11):
-    for j in range(0,4):
+for i in range(0,NUM_OF_PLANE):
+    for j in range(0,NUM_OF_G):
         if t[j,i]:
-            s+=np.sum(plane_4[i,2:5]*G_data_in[j,:])
+            s+=np.sum(plane_NUM_OF_G[i,2:5]*G_data_in[j,:])
 
 print(s)
 
-ran=np.array([0 for _ in range(0,11)])
-for i in range(0,11):
-    for j in range(0,4):
+ran=np.array([0 for _ in range(0,NUM_OF_PLANE)])
+for i in range(0,NUM_OF_PLANE):
+    for j in range(0,NUM_OF_G):
         if t[j,i]==1:
             ran[i]=j+1
 
