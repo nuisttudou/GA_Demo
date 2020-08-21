4968511fb9ac124963ae2c7f66980105ca2654c0
tudou <20171308085@nuist.edu.cn>
2020-08-21 17:01:13 +0800

    commit_all

--- Problem2/maxx.py
+++ Problem2/maxx.py
@@ -81,19 +81,7 @@ for i in range(0,NUM_OF_G):
     if can_use_category[park_category[i]].count(park_port_num[i])==0:
         can_use_category[park_category[i]].append(park_port_num[i])
 
-
-port_canuse_country=[set(can_use_country[0]),set(can_use_country[1])]
-port_canuse_category=[[],set(can_use_category[1]),set(can_use_category[2])
-                      ,set(can_use_category[3]),set(can_use_category[4])]
-
-port_equal_above_can_use=[set(),set(),set(),set(),set()]
-port_equal_above_can_use[4]=port_canuse_category[4]
-for i in [3,2,1]:
-    port_equal_above_can_use[i]=port_canuse_category[i] | port_equal_above_can_use[i+1]
-
-
-
-#%% 远机位
+#%%
 is_remote=(plane_out_time-plane_in_time)>=6/24
 plane_remote_index=[i for i in range(0,NUM_OF_PLANE) if is_remote[i]]
 plane_remote_num=len(plane_remote_index)
@@ -112,12 +100,10 @@ class P:
 #%%
 def create_individual(data):
     port_canuse_country=[set(can_use_country[0]),set(can_use_country[1])]
-    # port_canuse_category=[[],set(can_use_category[1]),set(can_use_category[2])
-    #                       ,set(can_use_category[3]),set(can_use_category[4])]
+    port_canuse_category=[[],set(can_use_category[1]),set(can_use_category[2])
+                          ,set(can_use_category[3]),set(can_use_category[4])]
     plane_have_port=[]
     used_prot_heap=[]
-    
-    plane_use_rear_index=[]
     for i in range(0,NUM_OF_PLANE):
         if is_remote[i]:
             continue
@@ -129,20 +115,11 @@ def create_individual(data):
                 port_canuse_country[0].add(top)
             if park_country[top]==1 or park_country[top]==2:
                 port_canuse_country[1].add(top)    
-        
-        # my_can_use=set() #当前候选机位
-        # for j in range(plane_category[i],5):#4为种类上限
-        #     my_can_use=my_can_use or port_canuse_category[j]
-        my_can_use=port_equal_above_can_use[plane_category[i]]
-        
-        my_can_use=my_can_use & port_canuse_country[plane_country[i]]
+        my_can_use=set() #当前候选机位
+        for j in range(plane_category[i],5):#4为种类上限
+            my_can_use=my_can_use or port_canuse_category[j]
+        my_can_use=my_can_use and port_canuse_country[plane_country[i]]
         my_can_use_list=list(my_can_use)
-        
-        # print(i,":",len(my_can_use))
-        if len(my_can_use)==0:
-            plane_use_rear_index.append(i)
-            continue
-        
         now_have_port_index=random.randint(0,len(my_can_use)-1)
         now_have_port=my_can_use_list[now_have_port_index]
         now_have_finished_time=plane_out_time[i]
@@ -152,9 +129,6 @@ def create_individual(data):
             port_canuse_country[1].remove(now_have_port)
         heapq.heappush(used_prot_heap,P(now_have_port,now_have_finished_time+5/60/24))# 5分钟安全
         plane_have_port.append(now_have_port)
-        
-    for i in plane_use_rear_index:
-        plane_have_port.insert(i,128)
     return plane_have_port
 
 #%%
@@ -166,15 +140,15 @@ MAXX=60078*20.58
 
 def pan(wait_check_plane):
     port_canuse_country=[set(can_use_country[0]),set(can_use_country[1])]
-    # port_canuse_category=[[],set(can_use_category[1]),set(can_use_category[2])
-    #                       ,set(can_use_category[3]),set(can_use_category[4])]
+    port_canuse_category=[[],set(can_use_category[1]),set(can_use_category[2])
+                          ,set(can_use_category[3]),set(can_use_category[4])]
     plane_have_port=[]
     used_prot_heap=[]
     
     for i in range(0,NUM_OF_PLANE_CALCULATE):
-        if is_remote[i] or wait_check_plane[i]==128:
+        if is_remote[i]:
             continue
-        now_intime=plane_in_time[i] 
+        now_intime=plane_in_time[i]
         while len(used_prot_heap)>0 and used_prot_heap[0].outtime<=now_intime:
             top_item=heapq.heappop(used_prot_heap)
             top=top_item.port_num
@@ -182,16 +156,14 @@ def pan(wait_check_plane):
                 port_canuse_country[0].add(top)
             if park_country[top]==1 or park_country[top]==2:
                 port_canuse_country[1].add(top)    
-                
-        # my_can_use=set() #当前候选机位
-        # for j in range(plane_category[i],5):#4为种类上限
-        #     my_can_use=my_can_use or port_canuse_category[j]
-        my_can_use=port_equal_above_can_use[plane_category[i]]
-        my_can_use=my_can_use & port_canuse_country[plane_country[i]]
+        my_can_use=set() #当前候选机位
+        for j in range(plane_category[i],5):#4为种类上限
+            my_can_use=my_can_use or port_canuse_category[j]
+        my_can_use=my_can_use and port_canuse_country[plane_country[i]]
         my_can_use_list=list(my_can_use)
         
-        
         t=0
+        
         for nu in plane_remote_index:
             if i>nu:
                 t+=1
@@ -234,7 +206,7 @@ def fitness3(plane_have_port, data):
 
 #%%
 ga = pyeasyga.GeneticAlgorithm(data,
-                                population_size=50,
+                                population_size=500,
                                 generations=50,
                                 crossover_probability=0.8,
                                 mutation_probability=0.05,
@@ -286,9 +258,9 @@ print("耗时:",t_fin-t_in,"s")
 print(pan(list(ga.best_individual()[1])))
 
 '''
-耗时: 17.361228704452515 s
-ans: 0.6608875621970746
-[228, 167, 264, 235, -1, 201, 254, 274, 169, 126, 225, 170, 255, 221, 130, 223, 230, 156, 278, 253, 232, 218, 127, 215, 203, 224, 171, 264, 231, 217, 210, 228, 207, 209, 270, 205, 128, 261, 213, 167, 117, 235, 110, 133, 256, 234, 155, 172, 118, 229, 261, 201, 255, 267, 216, 132, 278, 168, 258, 220, 271, 266, 219, 202, 130, 221, 259, 170, 269, 225, 270, 169, 227, 265, 207, 204, 107, 274, 277, 222, 217, 233, 268, 208, 235, 152, 273, 119, 209, 203, 122, 163, 131, 275, 160, 133, 167, 251, 252, 173, 171, 214, 230, 256, 250, 101, 110, 154, 257, 212, 201, 254, 125, 155, 234, 158, 166, 260, 126, 168, 221, 102, 279, 235, 162, 233, 276, 229, 258, 226, 129, 116, -1, 128, 207, 103, 132, 101, 111, 218, 263, 232, 205, 262, 105, 210, 208, 171, 125, 115, 256, 169, 214, 170, 225, 271, 211, 222, 204, 172, 112, 106, 270, 251, 269, 235, 146, 279, 151, 203, 129, 201, 219, 272, 127, 101, 258, 120, 124, 266, 217, 230, 265, 206, 207, 277, 213, 167, 215, 159, 229, 231, 220, 228, 211, 257, 221, 166, 225, 168, 121, 267, 156, 117, 172, 271, 171, 232, 253, 152, 208, 226, 274, 279, 209, 216, -1, 123, 126, 154, 275, 263, 130, 114, 218, 173, 222, 260, 204, 132, 131, 250, 125, 268, 224, 201, 251, 107, 233, 129, 207, 210, 133, 203, 170, 206, 259, 235, 265, 230, 228, 172, 267, 269, 128, 231, 253, 264, 254, 155, 212, 275, 227, 278, 219, 132, 118, 277, 154, 209, 202, 204, 217, 279, 208, 156, 213, 125, 234, 224, 260, 215, 146, 251, 270, 167, 274, 207, 226, 166, -1, 109, 265, 133, 205, 254, 253, 212, 273, 218, 263, 128, 169, 266, 155, 172, -1, 203, 271, 127, 227, 259, 262, 230, 208, 228, 101, 261, 160, 168, 201, 276, 202, 251, 252, 162, 274, 213, 129, 225, 204, 126, 235, 170, 120, 207, 233, 270, 229, 216, 226, 254, 157, 124, 221, 267, 132, 130, 264, 224, 171, 107, 206, 214, 222, 101, 165, 232, 257, 125, 110, 278, 105, 106, 148, 154, 255, 145, 211, 220, 256, 273, -1, -1]
-耗时: 17.367674827575684 s
+耗时: 214.58194947242737 s
+ans: 0.6770579531951816
+[269, 213, 209, 201, 133, 215, 211, 128, 202, 228, 230, 101, 274, 278, 265, 229, 169, 153, 131, 235, 266, 257, 220, 253, 132, 272, 165, 213, 256, 204, 207, 273, 124, 201, 255, 125, 218, 168, 234, 211, 157, 203, 109, 130, 230, 126, 128, 254, 151, 221, 261, 202, 260, 263, 232, 219, 265, 101, 173, 266, 250, 253, 267, 209, 133, 252, 231, 225, 132, 258, 207, 268, 278, 269, 218, 277, 149, 234, 205, 208, 274, 228, 201, 169, 254, 102, 224, 160, 262, 213, 112, 158, 272, 273, 110, 127, 256, 130, 223, 214, 227, 270, 259, 226, 219, 163, 111, 222, 215, 129, 173, 171, 170, 266, 255, 157, 101, 269, 204, 172, 265, 120, 155, 210, 116, 233, 278, 234, 232, 251, 261, 122, 203, 225, 127, 168, 202, 101, 113, 223, 212, 227, 263, 264, 156, 274, 206, 108, 271, 144, 131, 154, 256, 268, 259, 229, 267, 209, 215, 204, 168, 123, 228, 231, 273, 233, 104, 272, 109, 201, 130, 173, 170, 255, 172, 161, 133, 164, 171, 210, 230, 254, 222, 258, 278, 260, 276, 167, 211, 114, 226, 275, 216, 207, 212, 257, 205, 166, 261, 264, 123, 203, 158, 101, 221, 225, 153, 168, 234, 160, 206, 217, 131, 224, 271, 219, -1, 120, 155, 267, 214, 251, 229, 103, 252, 215, 227, 124, 268, 263, 213, 259, 202, 262, 278, 231, 235, 115, 126, 274, 272, 220, 253, 170, 112, 204, 203, 201, 230, 167, 273, 275, 261, 169, 269, 226, 154, 210, 127, 232, 279, 217, 125, 270, 233, 227, 121, 254, 221, 267, 132, 215, 211, 264, 234, 101, 260, 274, 265, 224, 133, 222, 110, 276, 223, 251, 209, 268, 219, 159, -1, 102, 275, 216, 128, 154, 173, 252, 258, 230, 250, 203, 214, 278, 255, 169, -1, 226, 217, 259, 221, 261, 167, 155, 211, 257, 163, 129, 152, 225, 168, 218, 213, 260, 171, 112, 274, 266, 277, 125, 234, 262, 124, 210, 162, 256, 209, 133, 208, 279, 233, 216, 156, 126, 263, 224, 228, 229, 220, 221, 212, 109, 276, 128, 259, 121, 144, 253, 215, 235, 118, 232, 113, 170, 101, 273, 207, 101, 130, 275, 202, 101, -1, -1]
+耗时: 214.5886514186859 s
 True
 '''
